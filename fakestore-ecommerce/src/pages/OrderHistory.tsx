c561a05f897f8fb83cde76d4736b03d55c59d8b6
MichaelRogers1978 <mikeangie3049@yahoo.com>
2025-11-08 17:04:37 -0700

    Remove unused imports to fix TypeScript build errors

--- fakestore-ecommerce/src/pages/OrderHistory.tsx
+++ fakestore-ecommerce/src/pages/OrderHistory.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 import { db } from '../config/firebaseConfig';
 import { useFirebaseAuth } from '../hooks/useFirebaseAuth';
 
